cb41bcb34ee28296654719309c1e1072439b0e29
underdark456 <bloodclaus@gmail.com>
2020-12-16 23:08:55 +0300

    Completed modulator test

--- JSON_POST.py
+++ JSON_POST.py
@@ -1,19 +1,25 @@
 import json
 import requests
 
-nms = '10.0.3.15'
-token = '46aa0a8c'
-out_type = json
-myurl = f'http://{nms}/jsonapi/?token={token}&out={out_type}'
+def star_hub_json():
+    nms = '10.0.3.15'
+    token = '46aa0a8c'
+    out_type = json
+    myurl = f'http://{nms}/jsonapi/?token={token}&out={out_type}'
+    data = {
+        "object": "controller",
+        "action": "select",
+        "id": "2"
+    }
+    return requests.post(myurl, json=data)
+def star_hub_txlvl():
+    resp = star_hub_json()
+    parsed = json.dumps(resp.json(), indent=4, sort_keys=True)
+    json_string_data = json.loads(parsed)
+    return float(json_string_data['data']['modulator_txlvl'])
 
-data = {
-    "object": "controller",
-    "action": "select",
-    "id": "2"
-}
 
-resp = requests.post(myurl, json=data)
-parsed = json.dumps(resp.json(), indent=4, sort_keys=True)
+
 
 # with open("data_file.json", "w") as write_file:
-#      write_file.write(parsed)
+#       write_file.write(parsed)
--- modulator.py
+++ modulator.py
@@ -2,18 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-
-response = requests.get('http://10.0.3.150/cm3?da=1')
-soup = BeautifulSoup(response.text, 'lxml')
-values = [element['value'] for element in soup.findAll('input', {'type':'text'})]
-print(values)
-
-
-
-# y = soup.findAll('input', {'type':'text'})
-# a = re.findall('(?<=value=")(\d*)',str(y))
-
-# print(a)
+def modulator_txlvl():
+    modulator_response = requests.get('http://10.0.3.150/cm3?da=1')
+    soup = BeautifulSoup(modulator_response.text, 'lxml')
+    values = [element['value'] for element in soup.findAll('input', {'type':'text'})]
+    return float(values[0] + '.' + values[1])
 
 # with open("modulator.html", "w",encoding='utf-8') as write_file:
 #     write_file.write(str(soup.prettify()))
